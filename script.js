a1e7410e33c24ab254be5f5dcc82ca41636437b3
chrisgoss <christinagoss206@gmail.com>
2020-06-17 15:29:31 -0700

    first skeleton commit

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("we loggin consoles");
+//https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+//https://www.w3schools.com/js/js_operators.asp
+//https://www.javascripttutorial.net/javascript-ternary-operator/
+
+
+
+document.getElementById("increment").addEventListener("mouseover", "click");
+
+document.getElementById("decrement").addEventListener("mouseover", "click");
+
+
+
+function increment value ++
+
+function decrement value --
+
+
+
+var display = display >= 0 ? "black" : "red";
