ad095126700c19deb1caa12deb1ad1db1488fbc6
chrisgoss <christinagoss206@gmail.com>
2020-06-17 17:10:03 -0700

    global variables

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ console.log("we loggin consoles");
 //https://www.w3schools.com/js/js_operators.asp
 //https://www.javascripttutorial.net/javascript-ternary-operator/
 
-
-
-document.getElementById("increment").addEventListener("mouseover", "click");
-
-document.getElementById("decrement").addEventListener("mouseover", "click");
-
-
-
-function increment value ++
-
-function decrement value --
+var display = document.getElementById("display");
+var input = document.getElementById("input");
+var increment = document.getElementById("increment");
+var decrement = document.getElementById("decrement");
 
 
 
-var display = display >= 0 ? "black" : "red";
+
+
+// document.getElementById("increment").addEventListener("mouseover", "click");
+// document.getElementById("decrement").addEventListener("mouseover", "click");
+
+// function increment() value ++
+// function decrement value --
+
+// var display = display >= 0 ? "black" : "red";
