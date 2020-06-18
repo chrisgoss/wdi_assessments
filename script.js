369c12183d69b9af50d3ead1fbbe7e78b11b8c94
chrisgoss <christinagoss206@gmail.com>
2020-06-17 18:01:31 -0700

    made a function with inoperable event listener

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ console.log("we loggin consoles");
 //https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 //https://www.w3schools.com/js/js_operators.asp
 //https://www.javascripttutorial.net/javascript-ternary-operator/
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments#:~:text=The%20arguments%20object%20is%20a,first%20entry's%20index%20at%200%20.
 
 var display = document.getElementById("display");
 var input = document.getElementById("input");
-var inc = document.getElementById("increment");
-var dec = document.getElementById("decrement");
+var inc = document.getElementById("inc");
+var dec = document.getElementById("inc");
 
-inc.addEventListener(onclick ? ++ : 0);
+function addOne(){
+    display.innerHTML = inc++;
+    console.log("inc worked");
+}
+inc.addEventListener(onclick, addOne);
 
-console.log("inc");
+
 
 
 // document.getElementById("increment").addEventListener("mouseover", "click");
