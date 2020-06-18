f4317fcc28a2fbe1249bc814dbc8b2e6736db4ce
chrisgoss <christinagoss206@gmail.com>
2020-06-17 17:35:06 -0700

    working on js event listeners

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 console.log("we loggin consoles");
+
 //https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 //https://www.w3schools.com/js/js_operators.asp
 //https://www.javascripttutorial.net/javascript-ternary-operator/
 
 var display = document.getElementById("display");
 var input = document.getElementById("input");
-var increment = document.getElementById("increment");
-var decrement = document.getElementById("decrement");
+var inc = document.getElementById("increment");
+var dec = document.getElementById("decrement");
 
+inc.addEventListener(onclick ? ++ : 0);
 
-
+console.log("inc");
 
 
 // document.getElementById("increment").addEventListener("mouseover", "click");
